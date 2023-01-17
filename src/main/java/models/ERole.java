ede8ebce1fc0afe47b146d181dec3796cf0f41e4
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-17 11:19:59 +0100

    user model

---
+++ src/main/java/models/ERole.java
@@ -0,0 +1,7 @@
+package models;
+
+public enum ERole {
+    ROLE_STUDENT,
+    ROLE_TEACHER,
+    ROLE_MAIN_TEACHER
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,10 @@
+package models;
+
+public class User {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private ERole role;
+}
