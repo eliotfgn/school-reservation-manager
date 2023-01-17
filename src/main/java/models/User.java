f0b513eb58cbbde72821206ed2da17d8b38d40e4
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-17 11:21:34 +0100

    user class constructor

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -7,4 +7,64 @@ public class User {
     private String email;
     private String password;
     private ERole role;
+
+    public User(Long id, String firstname, String lastname, String email, String password, ERole role) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ERole getRole() {
+        return role;
+    }
+
+    public void setRole(ERole role) {
+        this.role = role;
+    }
 }
